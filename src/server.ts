df5ff08a6493a09670a2a5bc9e95cf04a0d57443
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-22 23:32:14 +0300

    final commit

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { V0MODELS } from './controllers/model.index';
+import { sequelize } from './sequelize';
+import { IndexRouter } from './controllers/index.router';
+import { requireAuth } from './controllers/users/routes/auth.router';
+
+require('dotenv').config();
 
 (async () => {
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
   // Init the Express application
   const app = express();
 
@@ -11,24 +19,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-  
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
@@ -36,8 +26,11 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
+  app.use('/api/', IndexRouter)
 
-  app.get("/filteredimage/", async(req,res)=>{
+
+  app.get("/filteredimage/",requireAuth, async(req,res)=>{
+
     let {image_url}: any = req.query;
     if( !image_url ) {
       return res.status(400)
@@ -61,3 +54,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       console.log( `press CTRL+C to stop server` );
   } );
 })();
+
+
+
+ // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+  
+  //! END @TODO1
